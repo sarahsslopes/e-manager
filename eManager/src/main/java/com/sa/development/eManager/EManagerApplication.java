9a0939cbc101f6deb87218a2577c8d932dc94986
Sarah Lopes <sarah.ssl@ecxus.com.br>
2024-11-16 20:30:04 -0300

    Init project

---
+++ eManager/src/main/java/com/sa/development/eManager/EManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sa.development.eManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EManagerApplication.class, args);
+	}
+
+}
---
+++ eManager/src/test/java/com/sa/development/eManager/EManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.development.eManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
