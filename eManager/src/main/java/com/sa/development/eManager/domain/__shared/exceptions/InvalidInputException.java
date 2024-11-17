c4b4a9d71d9ea546d4464437bdf7840225eae426
Sarah Lopes <sarah.ssl@ecxus.com.br>
2024-11-17 10:11:42 -0300

    create exceptions base

--- eManager/src/main/java/com/sa/development/eManager/domain/__shared/exceptions/InvalidInputException.java
+++ eManager/src/main/java/com/sa/development/eManager/domain/__shared/exceptions/InvalidInputException.java
@@ -1,6 +1,6 @@
 package com.sa.development.eManager.domain.__shared.exceptions;
 
-public class InvalidInputException extends RuntimeException {
+public class InvalidInputException extends ValidationException {
 
     public InvalidInputException(String message) {
         super(message);
---
+++ eManager/src/main/java/com/sa/development/eManager/domain/__shared/exceptions/ValidationException.java
@@ -0,0 +1,12 @@
+package com.sa.development.eManager.domain.__shared.exceptions;
+
+public abstract class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
